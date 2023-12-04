93964315e556d4a433acab5b8bba64e9ecb8abc8
Marco Escaleira <marcoescaleira2000@gmail.com>
2023-12-04 18:38:51 +0000

    admin class and some small refactor

---
+++ app/src/main/java/com/example/thecaffeshop/Model/AdminDBHelper.kt
@@ -0,0 +1,94 @@
+package com.example.thecaffeshop.Model
+
+import android.annotation.SuppressLint
+import android.content.ContentValues
+import android.content.Context
+import android.database.Cursor
+import android.database.sqlite.SQLiteDatabase
+import android.database.sqlite.SQLiteOpenHelper
+
+private val DataBaseName = "TheCaffeShopDb.db"
+private val ver: Int = 1
+
+class AdminDBHelper(context: Context) : SQLiteOpenHelper(context, DataBaseName, null, ver) {
+
+    /* Customer Table */
+    public val TableName = "Admins"
+    public val Column_AdminId = "AdminId"
+    public val Column_AdminFullName = "AdminFullName"
+    public val Column_AdminEmail = "AdminEmail"
+    public val Column_AdminPhoneNo = "AdminPhoneNo"
+    public val Column_AdminUserName = "AdminUserName"
+    public val Column_AdminPassword = "AdminPassword"
+    public val Column_AdminIsActive = "AdminIsActive"
+
+    override fun onCreate(db: SQLiteDatabase?) {
+
+        val sqlCreateStatement: String = "CREATE TABLE " + TableName + " ( " + Column_AdminId +
+                " INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                Column_AdminFullName + " TEXT, " +
+                Column_AdminEmail + " TEXT, " +
+                Column_AdminPhoneNo + " TEXT, " +
+                Column_AdminUserName + " TEXT, " +
+                Column_AdminPassword + " TEXT, " +
+                Column_AdminIsActive + " BOOLEAN )"
+
+        db?.execSQL(sqlCreateStatement)
+    }
+
+    override fun onUpgrade(db: SQLiteDatabase?, oldVersion: Int, newVersion: Int) {
+        TODO("Not yet implemented")
+    }
+
+    fun registerAdmin(admin: User): Boolean {
+
+        // writableDatabase for insert actions
+        val db: SQLiteDatabase = this.writableDatabase
+        val cv: ContentValues = ContentValues()
+
+        cv.put(Column_AdminFullName, admin.fullName)
+        cv.put(Column_AdminEmail, admin.email)
+        cv.put(Column_AdminPhoneNo, admin.phoneNo)
+        cv.put(Column_AdminUserName, admin.cusUserName)
+        cv.put(Column_AdminPassword, admin.cusPassword)
+        cv.put(Column_AdminIsActive, admin.isActive)
+
+        val success = db.insert(TableName, null, cv)
+        db.close()
+        return success != -1L
+    }
+
+    @SuppressLint("Range")
+    fun getUserByUsername(username: String): User? {
+        val db: SQLiteDatabase = this.readableDatabase
+
+        val sqlStatement = "SELECT * FROM $TableName WHERE $Column_AdminUserName = ?"
+        val selectionArgs: Array<String> = arrayOf(username)
+
+        val cursor: Cursor = db.rawQuery(sqlStatement, selectionArgs)
+
+        val hasUser = cursor.moveToFirst()
+
+        val user: User
+        if (hasUser) {
+            val id = cursor.getInt(cursor.getColumnIndex(Column_AdminId))
+            val fullName = cursor.getString(cursor.getColumnIndex(Column_AdminFullName))
+            val email = cursor.getString(cursor.getColumnIndex(Column_AdminEmail))
+            val phoneNo = cursor.getString(cursor.getColumnIndex(Column_AdminPhoneNo))
+            val cusUserName = cursor.getString(cursor.getColumnIndex(Column_AdminUserName))
+            val cusPassword = cursor.getString(cursor.getColumnIndex(Column_AdminPassword))
+            val isActive = cursor.getInt(cursor.getColumnIndex(Column_AdminIsActive)) != 0
+
+            user = User(id, fullName, email, phoneNo, cusUserName, cusPassword, isActive, true)
+
+            return user
+        } else {
+            return null
+        }
+
+        // Close the cursor and database connection
+        cursor.close()
+        db.close()
+    }
+
+}
