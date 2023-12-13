ad3cada7baf766edfb675646d6889d1fac3717e3
Cesar Vieira <cesardiogovieira2003@gmail.com>
2023-12-13 23:35:22 +0000

    Added admin product list

--- app/src/main/java/com/example/thecaffeshop/ui/adminProducts/AdminProductsFragment.kt
+++ app/src/main/java/com/example/thecaffeshop/ui/adminProducts/AdminProductsFragment.kt
@@ -6,7 +6,12 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.findNavController
+import com.example.thecaffeshop.R
 import com.example.thecaffeshop.databinding.FragmentAdminProductsBinding
+import com.example.thecaffeshop.model.Product
+import com.example.thecaffeshop.ui.userStore.ProductsListAdapter
+import com.example.thecaffeshop.ui.userStore.StoreViewModel
 
 class AdminProductsFragment : Fragment() {
 
@@ -21,14 +26,35 @@ class AdminProductsFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val adminProductsViewModel =
-            ViewModelProvider(this).get(AdminProductsViewModel::class.java)
-
         _binding = FragmentAdminProductsBinding.inflate(inflater, container, false)
-        val root: View = binding.root
+        return binding.root
 
-        return root
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val storeViewModel =
+            ViewModelProvider(requireActivity()).get(StoreViewModel::class.java)
+
+        storeViewModel.products.observe(viewLifecycleOwner) { productsList ->
+            val adapter = ProductsListAdapter(
+                requireActivity().applicationContext,
+                layoutInflater,
+                productsList
+            );
+
+            binding.adminProductsListView.setOnItemClickListener() { adapterView, _, position, _ ->
+                val productAtPosition = adapterView.getItemAtPosition(position) as Product
+                storeViewModel.selectProduct(productAtPosition)
+                 view?.findNavController()
+           //         ?.navigate(R.id.action_navigation_user_store_to_productFragment)
+            }
+
+            binding.adminProductsListView.adapter = adapter
+        }
+    }
+
 
     override fun onDestroyView() {
         super.onDestroyView()
--- app/src/main/java/com/example/thecaffeshop/ui/adminProducts/AdminProductsViewModel.kt
+++ app/src/main/java/com/example/thecaffeshop/ui/adminProducts/AdminProductsViewModel.kt
@@ -1,9 +1,28 @@
 package com.example.thecaffeshop.ui.adminProducts
 
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.thecaffeshop.model.Product
+import com.example.thecaffeshop.model.ProductDBHelper
 
-class AdminProductsViewModel : ViewModel() {
+class AdminProductsViewModel(application: Application) : AndroidViewModel(application) {
+
+    private val productDBHelper: ProductDBHelper = ProductDBHelper(application.applicationContext)
+    private val _products = MutableLiveData<ArrayList<Product>>()
+    val products: LiveData<ArrayList<Product>> = _products
+
+    init {
+        _products.value = productDBHelper.getAllProducts()
+    }
+
+    private val _product = MutableLiveData<Product>()
+    val product: LiveData<Product> = _product
+
+    fun selectProduct (product: Product) {
+        _product.value = product
+    }
 
 }
