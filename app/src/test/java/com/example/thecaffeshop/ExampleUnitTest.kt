5a3b1352b10b0fd4b9182fde13896b580ad41f2c
Marco Escaleira <marcoescaleira2000@gmail.com>
2024-01-07 01:02:31 +0000

    example unit test

--- app/src/test/java/com/example/thecaffeshop/ExampleUnitTest.kt
+++ app/src/test/java/com/example/thecaffeshop/ExampleUnitTest.kt
@@ -1,5 +1,6 @@
 package com.example.thecaffeshop
 
+import com.example.thecaffeshop.model.Product
 import org.junit.Test
 
 import org.junit.Assert.*
@@ -9,9 +10,21 @@ import org.junit.Assert.*
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
-class ExampleUnitTest {
+class ObjectsUnitTests {
     @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
+    fun isProductModelCorrect() {
+        assertEquals(
+            Product(
+                1,
+                "Coffee",
+                "Delicious coffee",
+                2.5,
+                "imageUrl",
+                true,
+                3,
+                arrayListOf()
+            ).toString(),
+            "Product(prodId='1', prodName='Coffee', prodDescription='Delicious coffee', prodPrice='2.5', prodImage='imageUrl', prodAvailable='true', prodRating='3', comments='[]')"
+        )
     }
 }
